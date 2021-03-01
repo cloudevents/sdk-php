213309ece8813a652927b267d8e99584ed35a2b9
Graham Campbell <hello@gjcampbell.co.uk>
2021-03-01 20:44:49 +0000

    Create ArraySerializer
    Signed-off-by: Graham Campbell <hello@gjcampbell.co.uk>

--- src/CloudEvents/Serializers/JsonSerializer.php
+++ src/CloudEvents/Serializers/JsonSerializer.php
@@ -4,69 +4,22 @@ namespace CloudEvents\Serializers;
 
 use CloudEvents\CloudEventInterface;
 use CloudEvents\Serializers\Exceptions\UnsupportedEventSpecVersionException;
-use CloudEvents\V1\CloudEventInterface as V1CloudEventInterface;
-use DateTimeInterface;
-
-use function array_merge;
 
 class JsonSerializer
 {
+    protected ArraySerializer $arraySerializer;
+
+    public function __construct(ArraySerializer $arraySerializer = null)
+    {
+        $this->arraySerializer = $arraySerializer ?? new ArraySerializer();
+    }
+
     /**
      * @throws UnsupportedEventSpecVersionException
      * @throws \JsonException
      */
     public function serialize(CloudEventInterface $cloudEvent): string
     {
-        $payload = $this->createPayload($cloudEvent);
-        $payload = array_merge($payload, $this->formatData($cloudEvent->getData()));
-
-        return json_encode($payload, JSON_THROW_ON_ERROR);
-    }
-
-    /**
-     * Get a JSON-serializable array representation of the CloudEvent.
-     *
-     * @throws UnsupportedEventSpecVersionException
-     */
-    protected function createPayload(CloudEventInterface $cloudEvent): array
-    {
-        if ($cloudEvent instanceof V1CloudEventInterface) {
-            return [
-                'specversion' => $cloudEvent->getSpecVersion(),
-                'id' => $cloudEvent->getId(),
-                'type' => $cloudEvent->getType(),
-                'datacontenttype' => $cloudEvent->getDataContentType(),
-                'dataschema' => $cloudEvent->getDataSchema(),
-                'subject' => $cloudEvent->getSubject(),
-                'time' => $this->formatTime($cloudEvent->getTime()),
-            ];
-        }
-
-        throw new UnsupportedEventSpecVersionException();
-    }
-
-    private function formatTime(?DateTimeInterface $time): ?string
-    {
-        return $time === null ? null : $time->format(DATE_RFC3339);
-    }
-
-    /**
-     * @param mixed|null $data
-     */
-    private function formatData($data): array
-    {
-        if ($this->isBinary($data)) {
-            return ['data_base64' => base64_encode($data)];
-        }
-
-        return ['data' => $data];
-    }
-
-    /**
-     * @param mixed|null $data
-     */
-    private function isBinary($data): bool
-    {
-        return is_string($data) && !preg_match('//u', $data);
+        return json_encode($this->arraySerializer->serialize($cloudEvent), JSON_THROW_ON_ERROR);
     }
 }
---
+++ tests/CloudEvents/Serializers/ArraySerializerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CloudEvents\Tests\CloudEvents\Serializers;
+
+use CloudEvents\Serializers\ArraySerializer;
+use CloudEvents\V1\CloudEventInterface;
+use DateTimeImmutable;
+use PHPUnit\Framework\MockObject\Stub;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \CloudEvents\Serializers\ArraySerializer
+ */
+class ArraySerializerTest extends TestCase
+{
+    /**
+     * @covers ::serialize
+     */
+    public function testSerialize(): void
+    {
+        /** @var CloudEventInterface|Stub $event */
+        $event = $this->createStub(CloudEventInterface::class);
+        $event->method('getSpecVersion')->willReturn('1.0');
+        $event->method('getId')->willReturn('1234-1234-1234');
+        $event->method('getType')->willReturn('com.example.someevent');
+        $event->method('getDataContentType')->willReturn('application/json');
+        $event->method('getDataSchema')->willReturn('com.example/schema');
+        $event->method('getSubject')->willReturn('larger-context');
+        $event->method('getTime')->willReturn(new DateTimeImmutable('2018-04-05T17:31:00Z'));
+        $event->method('getData')->willReturn(['key' => 'value']);
+
+        $formatter = new ArraySerializer();
+
+        $this->assertSame(
+            [
+                'specversion' => '1.0',
+                'id' => '1234-1234-1234',
+                'type' => 'com.example.someevent',
+                'datacontenttype' => 'application/json',
+                'dataschema' => 'com.example/schema',
+                'subject' => 'larger-context',
+                'time' => '2018-04-05T17:31:00+00:00',
+                'data' => [
+                    'key' => 'value',
+                ]
+            ],
+            $formatter->serialize($event)
+        );
+    }
+}

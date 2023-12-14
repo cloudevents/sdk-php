c1cdba809ab18d1ea7a7afea6848b53feffb562e
Graham Campbell <hello@gjcampbell.co.uk>
2023-12-14 02:45:21 +0000

    Enable configuration of subsecond precision of timestamps
    Signed-off-by: Graham Campbell <hello@gjcampbell.co.uk>

--- src/Serializers/Normalizers/V1/Normalizer.php
+++ src/Serializers/Normalizers/V1/Normalizer.php
@@ -11,12 +11,25 @@ use CloudEvents\Utilities\DataFormatter;
 final class Normalizer implements NormalizerInterface
 {
     /**
+     * @var array{subsecondPrecision?: int<0, 6>}
+     */
+    private array $configuration;
+
+    /**
+     * @param array{subsecondPrecision?: int<0, 6>} $configuration
+     */
+    public function __construct(array $configuration = [])
+    {
+        $this->configuration = $configuration;
+    }
+
+    /**
      * @return array<string, mixed>
      */
     public function normalize(CloudEventInterface $cloudEvent, bool $rawData): array
     {
         return array_merge(
-            AttributeConverter::toArray($cloudEvent),
+            AttributeConverter::toArray($cloudEvent, $this->configuration),
             DataFormatter::encode($cloudEvent->getData(), $rawData)
         );
     }
